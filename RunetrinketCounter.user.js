9f9813e8e9a6f14976f17f754a3ea881c9ece2cb
stellar-demesne <54080611+stellar-demesne@users.noreply.github.com>
2024-08-28 03:18:10 -0700

    Create RunetrinketCounter.user.js

---
+++ RunetrinketCounter.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Runetrinket-Counter
+// @namespace    https://github.com/stellar-demesne/Trimps-RunetrinketCounter
+// @version      1.0
+// @updateURL    https://github.com/stellar-demesne/Trimps-RunetrinketCounter/RunetrinketCounter.user.js
+// @description  Runetrinket Counter for Trimps
+// @author       StellarDemesne
+// @include      *trimps.github.io*
+// @include      *kongregate.com/games/GreenSatellite/trimps
+// @grant        none
+// ==/UserScript==
+var script = document.createElement('script');
+script.id = 'Runetrinket-Counter';
+script.src = 'https://github.com/stellar-demesne/Trimps-RunetrinketCounter/RunetrinketCounter.js';
+script.setAttribute('crossorigin', "anonymous");
+document.head.appendChild(script);
