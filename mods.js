45d28e97b97dc784d29dbb6695c5535e455222de
stellar-demesne <54080611+stellar-demesne@users.noreply.github.com>
2024-08-28 03:29:58 -0700

    fix broken link in mods.js

--- mods.js
+++ mods.js
@@ -1,5 +1,5 @@
 var script = document.createElement('script');
 script.id = 'Runetrinket-Counter';
-script.src = 'https://github.com/stellar-demesne/Trimps-RunetrinketCounter/RunetrinketCounter.js';
+script.src = 'https://stellar-demesne.github.io/Trimps-RunetrinketCounter/RunetrinketCounter.js';
 script.setAttribute('crossorigin', "anonymous");
 document.head.appendChild(script);
