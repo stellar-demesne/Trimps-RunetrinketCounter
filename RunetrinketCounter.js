c76c74014bde9a0a345c5441b52a99e02e1c042c
stellar-demesne <54080611+stellar-demesne@users.noreply.github.com>
2024-08-27 22:32:16 -0700

    Create RunetrinketCounter.js

---
+++ RunetrinketCounter.js
@@ -0,0 +1,85 @@
+
+
+if (document.getElementById('RunetrinketCounter') === null) {
+    const containerRunetrinketCounter = document.createElement('DIV');
+  
+    let darkmode_colours = ' color: rgb(0,0,0); background-color: rgb(93,93,93);'
+    // TODO: add other colours which look decent for the other options
+    chosen_colours = darkmode_colours;
+    // TODO: add detection for when to apply other colours
+    containerRunetrinketCounter.setAttribute('style', 'display: block; float: right; width: 30%; font-size: 0.8vw; text-align: center;' + chosen_colours);
+    const textareaRunetrinketCounter = document.createElement('SPAN');
+    containerRunetrinketCounter.setAttribute('onmouseover', populateRunetrinketCounterTooltip(true));
+    containerRunetrinketCounter.setAttribute('onmouseout', 'tooltip("hide")');
+    textareaRunetrinketCounter.id = 'RunetrinketCounter';
+    containerRunetrinketCounter.appendChild(textareaRunetrinketCounter);
+    let target_area = document.getElementById('wood');
+    target_area.insertBefore(containerRunetrinketCounter, target_area.children[0]);
+}
+
+if (game.portal.Observation.radLocked == false) {
+    populateRunetrinketCounterInfo();
+    setInterval( function () {
+        populateRunetrinketCounterInfo();
+    }, 10000);
+}
+
+
+function getRunetrinketCountFromGame() {
+    return game.portal.Observation.trinkets;
+}
+
+function getRunetrinketMaxFromGame() {
+    let trinkets_max = (game.portal.Observation.radLevel + 1) * game.portal.Observation.trinketsPerLevel;
+    if (game.global.u2MutationData !== { } && game.global.u2MutationData.Runed) {
+        trinkets_max = trinkets_max * 1.5;
+    };
+    return trinkets_max;
+}
+
+function getRunetrinketEffect() {
+    let trinkets = getRunetrinketCountFromGame()
+    let trinkmax = getRunetrinketMaxFromGame()
+    if (trinkmax < trinkets) trinkets = trinkmax;
+
+    const effectiveness_per = game.portal.Observation.radLevel + 1;
+    const effectiveness = trinkets * effectiveness_per;
+    return prettify(effectiveness);
+}
+
+function getRunetrinketGuaranteedRate() {
+    let perklevels = game.portal.Observation.radLevel;
+    let halved = Math.floor(perklevels / 2)
+    return prettify(halved);
+}
+
+function makeStringForDisplay() {
+    const runetrinketstring = getRunetrinketCountFromGame() + "<br\>/ " + getRunetrinketMaxFromGame();
+    return runetrinketstring
+}
+
+function populateRunetrinketCounterTooltip() {
+    if (usingRealTimeOffline == true) {
+      return '';
+    }
+  
+    let tooltipstring = '';
+    tooltipstring = "tooltip('Runetrinket Summary', 'customText', event, '";
+    tooltipstring += `<p>Runetrinkets give 1% per runetrinket per perk level, for a current boost of <b>` + getRunetrinketEffect() + `%</b>.</p>`;
+
+    tooltipstring += game.portal.Observation.getChanceText();
+    tooltipstring += ` Also, you are getting a guaranteed <b>` + getRunetrinketGuaranteedRate() + `</b> every 25 zones past z100.</p>`;
+    tooltipstring += "')"
+    return tooltipstring
+}
+
+function populateRunetrinketCounterInfo() {
+    if (usingRealTimeOffline == true) {
+      return '';
+    }
+  
+    const target_element = document.getElementById('RunetrinketCounter');
+    const the_information = makeStringForDisplay();
+    target_element.innerHTML = the_information;
+    target_element.parentNode.setAttribute('onmouseover', populateRunetrinketCounterTooltip());
+}
