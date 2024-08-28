c99d644d20ceac1c5356151c89d52c5be2b7c754
stellar-demesne <54080611+stellar-demesne@users.noreply.github.com>
2024-08-28 03:18:05 -0700

    Create mods.js

---
+++ mods.js
@@ -0,0 +1,5 @@
+var script = document.createElement('script');
+script.id = 'Runetrinket-Counter';
+script.src = 'https://github.com/stellar-demesne/Trimps-RunetrinketCounter/RunetrinketCounter.js';
+script.setAttribute('crossorigin', "anonymous");
+document.head.appendChild(script);
